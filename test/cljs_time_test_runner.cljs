3b1d524429f6fd948f99b3452ff9a837f05d41d2
Gabriel Horner <gabriel@logseq.com>
2022-04-29 10:25:41 -0400

    Process should fail when tests fail

--- test/cljs_time_test_runner.cljs
+++ test/cljs_time_test_runner.cljs
@@ -3,6 +3,11 @@
             [nbb.core :as nbb]
             [cljs-time-format-test]))
 
+(defmethod t/report [::t/default :end-run-tests] [{:keys [error fail]}]
+  (if (pos? (+ error fail))
+    (js/process.exit 1)
+    (js/process.exit 0)))
+
 (defn init []
   (t/run-tests 'cljs-time-format-test))
 
