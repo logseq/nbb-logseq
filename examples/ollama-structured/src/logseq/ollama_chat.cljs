6253d063e6689714981c28ef5fdbe46a797a9fbd
Gabriel Horner <gabriel@logseq.com>
2025-02-23 03:06:14 -0500

    basic chat works for any schema class!

--- examples/ollama-structured/src/logseq/ollama_chat.cljs
+++ examples/ollama-structured/src/logseq/ollama_chat.cljs
@@ -10,11 +10,25 @@
             ["os" :as os]))
 
 (def user-config
-  {:schema.class/Movie [:schema.property/actor
-                        :schema.property/director
-                        :schema.property/musicBy
-                        :schema.property/datePublished
-                        :schema.property/url]})
+  {:schema.class/Movie
+   {:schema.property/actor
+    {:build/tags [:schema.class/Person] :chat-properties [:schema.property/url]}
+    :schema.property/director
+    {:build/tags [:schema.class/Person] :chat-properties [:schema.property/url]}
+    :schema.property/musicBy
+    {:build/tags [:schema.class/Person] :chat-properties [:schema.property/url]}
+    :schema.property/datePublished {}
+    :schema.property/url {}}
+   :schema.class/Book
+   {:schema.property/author
+    {:build/tags [:schema.class/Person] :chat-properties [:schema.property/url]}
+    :schema.property/datePublished {}}
+   :schema.class/MusicRecording
+   {:schema.property/byArtist
+    {:build/tags [:schema.class/Person] :chat-properties [:schema.property/url]}
+    :schema.property/inAlbum
+    {:build/tags [:schema.class/MusicAlbum] :chat-properties [:schema.property/url]}
+    :schema.property/datePublished {}}})
 
 (defn- get-dir-and-db-name
   "Gets dir and db name for use with open-db!"
@@ -25,30 +39,35 @@
       ((juxt node-path/dirname node-path/basename) graph-dir'))
     [(node-path/join (os/homedir) "logseq" "graphs") graph-dir]))
 
-(defn- ->chat-properties [graph-properties]
+(defn define-chat-property [input-class graph-properties prop-ident]
+  (case (get-in graph-properties [prop-ident :logseq.property/type])
+    :node
+    (let [obj-properties (get-in user-config [input-class prop-ident :chat-properties])]
+      {:type :object
+       :properties
+       (merge {:name {:type :string}}
+              (->> obj-properties
+                   (map #(vector % (define-chat-property input-class graph-properties %)))
+                   (into {})))
+       :required (vec (concat [:name] obj-properties))})
+    :date
+    {:type :string :format :date}
+    {:type :string}))
+
+(defn- ->chat-properties [input-class graph-properties]
   (merge {:name {:type :string}}
          (->> graph-properties
               (map (fn [[k v]]
-                     (let [chat-property
-                           (case (get-in graph-properties [k :logseq.property/type])
-                             :node
-                             {:type :object
-                              :properties
-                              {:name {:type :string}
-                               :schema.property/url {:type :string}}
-                              :required [:name :schema.property/url]}
-                             :date
-                             {:type :string :format :date}
-                             {:type :string})]
+                     (let [chat-property (define-chat-property input-class graph-properties k)]
                        [k
                         (if (= :db.cardinality/many (:db/cardinality v))
                           {:type :array :items chat-property}
                           chat-property)])))
               (into {}))))
 
-(defn- ->post-body [graph-properties args]
-  (let [prompt (str "Tell me about movie " (pr-str (first args)))
-        chat-properties (->chat-properties graph-properties)]
+(defn- ->post-body [input-class graph-properties args]
+  (let [prompt (str "Tell me about " (first args) " " (pr-str (string/join " " (rest args))))
+        chat-properties (->chat-properties input-class graph-properties)]
     {:model "llama3.2"
      :messages [{:role "user" :content prompt}]
      :stream false
@@ -57,7 +76,7 @@
               :required (into [:name] (keys graph-properties))}}))
 
 (defn- print-export-map
-  [body graph-properties]
+  [body input-class graph-properties]
   (let [content-json (.. body -message -content)
         content (-> (js/JSON.parse content-json)
                     (js->clj :keywordize-keys true))
@@ -69,8 +88,9 @@
                            (fn [e]
                              (case (get-in graph-properties [k :logseq.property/type])
                                :node
-                               [:build/page (cond-> {:block/title (:name e)
-                                                     :build/tags [:schema.class/Person]}
+                               [:build/page (cond-> {:block/title (:name e)}
+                                              (get-in user-config [input-class k :build/tags])
+                                              (assoc :build/tags (get-in user-config [input-class k :build/tags]))
                                               (seq (dissoc e :name))
                                               (assoc :build/properties (dissoc e :name)))]
                                :date
@@ -80,20 +100,21 @@
                          (set (map prop-value v))
                          (prop-value v)))]))
              (into {}))
-        graph-classes {:schema.class/Person {}
-                       :schema.class/Movie {}}
+        graph-classes (merge (zipmap (distinct (mapcat :build/tags (vals (get user-config input-class))))
+                                     (repeat {}))
+                             {input-class {}})
         export-map
         {:properties graph-properties
          :classes graph-classes
          :pages-and-blocks [{:page {:build/journal 20250221}
                              :blocks [{:block/title (:name content)
-                                       :build/tags [:schema.class/Movie]
+                                       :build/tags [input-class]
                                        :build/properties obj-properties}]}]}]
     (pprint/pprint export-map)))
 
 (defn- structured-chat
-  [graph-properties args]
-  (let [post-body (->post-body graph-properties args)
+  [input-class graph-properties args]
+  (let [post-body (->post-body input-class graph-properties args)
         post-body' (clj->js post-body :keyword-fn #(subs (str %) 1))]
     (-> (p/let [resp (js/fetch "http://localhost:11434/api/chat"
                                #js {:method "POST"
@@ -101,7 +122,7 @@
                                     :body (js/JSON.stringify post-body')})
                 body (.json resp)]
           (if (= 200 (.-status resp))
-            (print-export-map body graph-properties)
+            (print-export-map body input-class graph-properties)
             (do
               (println "Error: Chat endpoint returned" (.-status resp) "with message" (pr-str (.-error body)))
               (js/process.exit 1))))
@@ -115,6 +136,10 @@
    :verbose {:alias :v
              :desc "Print more info"}})
 
+(defn- error [msg]
+  (println (str "Error: " msg))
+  (js/process.exit 1))
+
 (defn -main [& args]
   (let [[graph-dir & args'] args
         options (cli/parse-opts args' {:spec spec})
@@ -124,11 +149,23 @@
             (js/process.exit 1))
         [dir db-name] (get-dir-and-db-name graph-dir)
         conn (sqlite-cli/open-db! dir db-name)
-        graph-properties (->> (:schema.class/Movie user-config)
-                              (map #(d/entity @conn %))
-                              (map #(vector (:db/ident %) (select-keys % [:logseq.property/type :db/cardinality])))
+        input-class
+        (or (->>
+             (d/q '[:find [(pull ?b [:db/ident]) ...]
+                    :in $ ?name
+                    :where [?b :block/tags :logseq.class/Tag] [?b :block/name ?name]]
+                  @conn
+                  (string/lower-case (first args')))
+             first
+             :db/ident)
+            (error (str "No class found for" (pr-str (first args')))))
+        graph-properties (->> (keys (input-class user-config))
+                              (map #(or (d/entity @conn %)
+                                        (error (str "No property exists for " (pr-str %)))))
+                              (map #(vector (:db/ident %)
+                                            (select-keys % [:logseq.property/type :db/cardinality])))
                               (into {}))]
     (when (:verbose options) (println "DB contains" (count (d/datoms @conn :eavt)) "datoms"))
-    (structured-chat graph-properties args')))
+    (structured-chat input-class graph-properties args')))
 
 #js {:main -main}
