723c0367ca8864ee3444769983bdd68917ba392e
Gabriel Horner <gabriel@logseq.com>
2025-08-13 15:16:25 -0400

    self-hosted core.match fork w/ macro rewritten seems to work

--- features/core.match/src/nbb/impl/core_match.cljs
+++ features/core.match/src/nbb/impl/core_match.cljs
@@ -1,12 +1,31 @@
 (ns nbb.impl.core-match
-  {:no-doc true}
-  (:require [cljs.core.match :as match]
-            [nbb.core :as nbb]
-            [sci.core :as sci]))
+  (:require [sci.core :as sci]
+            ;; self-hostable core.match (e.g. abbinare)
+            [cljs.core.match :as m]
+            [nbb.core :as nbb]))
 
-(def core-ns (sci/create-ns 'cljs.core.match nil))
+(def m-ns (sci/create-ns 'cljs.core.match nil))
+
+;; SCI macro: first two args are &form and &env
+;; Macro is mostly a copy of https://github.com/clojure/core.match/blob/753e8a30de9c487ceba8ff7d39435bf87edff505/src/main/clojure/cljs/core/match.clj#L19-L32
+(defn ^:sci/macro match
+  [_&form _&env vars & clauses]
+  (let [[vars clauses]
+        (if (vector? vars)
+          [vars clauses]
+          [(vector vars)
+           (mapcat (fn [[c a]]
+                     [(if (not= c :else) (vector c) c) a])
+                   (partition 2 clauses))])]
+    ;; Unlike original macro, this one doesn't have a *clojurescript* binding. Dunno if it's needed
+    (binding [m/*line*   (-> _&form meta :line)
+              m/*locals* (dissoc (:locals _&env) '_)
+              m/*warned* (atom false)]
+      (m/clj-form vars clauses))))
+
 (def core-namespace
-  {'match (sci/copy-var match/match core-ns)})
+  {'match (sci/copy-var match m-ns)
+   'backtrack (sci/copy-var m/backtrack m-ns)})
 
 (def config {:namespaces {'cljs.core.match core-namespace}})
 
